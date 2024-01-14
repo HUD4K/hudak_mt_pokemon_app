33902a249bb89c79a4e9b01be6788e72c2286403
HUD4K <mh.michalhudak@gmail.com>
2024-01-14 23:30:59 +0100

    History fragment tab

--- app/src/main/java/com/hudak/mthudakpokemon/view/PokemonHistoryFragment.kt
+++ app/src/main/java/com/hudak/mthudakpokemon/view/PokemonHistoryFragment.kt
@@ -1,4 +1,39 @@
 package com.hudak.mthudakpokemon.view
 
-class PokemonHistoryFragment {
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.hudak.mthudakpokemon.R
+import com.hudak.mthudakpokemon.SharedPreferencesRepository
+
+class PokemonHistoryFragment : Fragment() {
+    private lateinit var nameList: TextView
+    private lateinit var sharedPreferencesRepository: SharedPreferencesRepository
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        sharedPreferencesRepository = SharedPreferencesRepository(requireContext())
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.fragment_page2, container, false)
+        nameList = view.findViewById(R.id.historylist)
+
+        loadSavedPokemons()
+
+        return view
+    }
+
+    override fun onResume() {
+        super.onResume()
+        loadSavedPokemons()
+    }
+
+    private fun loadSavedPokemons() {
+        val pokemonNames = sharedPreferencesRepository.getAllPokemonNames()
+        nameList.text = pokemonNames.joinToString("\n")
+    }
 }
